39af5d9e7c0145485aa171227752529deab2a094
AntonLiutov <aliutov@kse.org.ua>
2022-05-14 20:43:16 +0300

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://2e73efd7.us-south.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
